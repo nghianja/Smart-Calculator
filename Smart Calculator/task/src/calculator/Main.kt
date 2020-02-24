f1b9b067a8befd365dfdb833353b027508ebdb18
James Hian Ng <james.ng@projapps.com>
2020-02-24 14:44:02 +0800

    Complete Stage #2

--- Smart Calculator/task/src/calculator/Main.kt
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -1,10 +1,20 @@
 package calculator
 
-import java.util.*
+import java.util.Scanner
 
 fun main() {
     val scanner = Scanner(System.`in`)
-    val n1 = scanner.nextInt()
-    val n2 = scanner.nextInt()
-    println(n1 + n2)
+    loop@ while (true) {
+        val line = scanner.nextLine().trim()
+        if (line.length == 0) continue
+        val tokens = line.split(" ")
+        when (tokens.size) {
+            1 -> {
+                    if (tokens[0] == "/exit") break@loop
+                    println(tokens[0].toInt())
+            }
+            2 -> println(tokens[0].toInt() + tokens[1].toInt())
+        }
+    }
+    println("Bye!")
 }
