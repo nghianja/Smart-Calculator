e5c2da295a0436cf7a71c6aede5061694b971c05
James Hian Ng <james.ng@projapps.com>
2020-02-24 23:54:21 +0800

    Complete Stage #5

--- Smart Calculator/task/src/calculator/Main.kt
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -6,28 +6,35 @@ fun isMinus(token: String): Boolean {
     return (token[0] == '-' && token.length % 2 == 1)
 }
 
+fun printSum(line: String) {
+    val tokens = line.split("\\s+".toRegex())
+    var sum = tokens[0].toInt()
+    for (i in 1 until tokens.size step 2) {
+        if (isMinus(tokens[i]))
+            sum -= tokens[i + 1].toInt()
+        else
+            sum += tokens[i + 1].toInt()
+    }
+    println(sum)
+}
+
 fun main() {
     val scanner = Scanner(System.`in`)
     loop@ while (true) {
         val line = scanner.nextLine().trim()
         if (line.length == 0) continue
-        val tokens = line.split("\\s+".toRegex())
-        if (tokens.size == 1) {
-            when (tokens[0]) {
+        else if (line.startsWith('/')) {
+            when (line) {
                 "/exit" -> break@loop
                 "/help" -> println("The program calculates the sum of numbers. It supports both unary and binary minus operators.")
-                else -> println(tokens[0].toInt())
+                else -> println("Unknown command")
             }
-        }
-        else {
-            var sum = tokens[0].toInt()
-            for (i in 1 until tokens.size step 2) {
-                if (isMinus(tokens[i]))
-                    sum -= tokens[i + 1].toInt()
-                else
-                    sum += tokens[i + 1].toInt()
+        } else {
+            try {
+                printSum(line)
+            } catch (e: Exception) {
+                println("Invalid expression")
             }
-            println(sum)
         }
     }
     println("Bye!")
