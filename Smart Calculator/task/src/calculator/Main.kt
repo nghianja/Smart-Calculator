0fcca5402a94bdef3bc41d02d1f5f9856f8e4f13
James Hian Ng <james.ng@projapps.com>
2020-02-25 15:41:56 +0800

    Complete Stage #6

--- Smart Calculator/task/src/calculator/Main.kt
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -2,40 +2,87 @@ package calculator
 
 import java.util.Scanner
 
+val assignments = mutableMapOf<String, Int>()
+
+fun isAlpha(line: String): Boolean {
+    for (i in line.indices) {
+        if (!line[i].isLetter())
+            return false
+    }
+    return true
+}
+
+fun isAssignment(line: String): Boolean {
+    val tokens = line.split("=")
+    if (tokens.size == 2) {
+        val key = tokens[0].trim()
+        if (!isAlpha(key)) {
+            throw Exception("Invalid identifier")
+        }
+        val valstr = tokens[1].trim()
+        val value = if (!isAlpha(valstr)) {
+            valstr.toIntOrNull() ?: throw Exception("Invalid assignment")
+        } else {
+            assignments[valstr] ?: throw Exception("Unknown variable")
+        }
+        assignments[key] = value
+        return true
+    } else if (tokens.size > 2) {
+        throw Exception("Invalid assignment")
+    }
+    return false
+}
+
+fun isCommand(line: String): Boolean {
+    if (line.startsWith('/')) {
+        when (line) {
+            "/exit" -> throw Exception("Bye!")
+            "/help" -> println("The program calculates the sum of numbers. It supports both unary and binary minus operators.")
+            else -> throw Exception("Unknown command")
+        }
+        return true
+    }
+    return false
+}
+
 fun isMinus(token: String): Boolean {
     return (token[0] == '-' && token.length % 2 == 1)
 }
 
 fun printSum(line: String) {
     val tokens = line.split("\\s+".toRegex())
-    var sum = tokens[0].toInt()
+    var sum = if (!isAlpha(tokens[0])) {
+        tokens[0].toIntOrNull() ?: throw Exception("Invalid expression")
+    } else {
+        assignments[tokens[0]] ?: throw Exception("Unknown variable")
+    }
     for (i in 1 until tokens.size step 2) {
+        val valstr = tokens[i + 1]
+        val value = if (!isAlpha(valstr)) {
+            valstr.toIntOrNull() ?: throw Exception("Invalid expression")
+        } else {
+            assignments[valstr] ?: throw Exception("Unknown variable")
+        }
         if (isMinus(tokens[i]))
-            sum -= tokens[i + 1].toInt()
+            sum -= value
         else
-            sum += tokens[i + 1].toInt()
+            sum += value
     }
     println(sum)
 }
 
 fun main() {
     val scanner = Scanner(System.`in`)
-    loop@ while (true) {
+    while (true) {
         val line = scanner.nextLine().trim()
-        if (line.length == 0) continue
-        else if (line.startsWith('/')) {
-            when (line) {
-                "/exit" -> break@loop
-                "/help" -> println("The program calculates the sum of numbers. It supports both unary and binary minus operators.")
-                else -> println("Unknown command")
-            }
-        } else {
-            try {
-                printSum(line)
-            } catch (e: Exception) {
-                println("Invalid expression")
-            }
+        try {
+            if (line.isEmpty() || isCommand(line) || isAssignment(line))
+                continue
+            printSum(line)
+        } catch (e: Exception) {
+            println(e.message)
+            if (e.message == "Bye!")
+                break
         }
     }
-    println("Bye!")
 }
