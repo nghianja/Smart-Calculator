4360b8fafb031ce488beea8282dab9bbf9f7eeb2
James Hian Ng <james.ng@projapps.com>
2020-05-07 20:22:27 +0800

    Completed Python Stage #2

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -1,3 +1,12 @@
 # write your code here
-numbers = input().split()
-print(int(numbers[0]) + int(numbers[1]))
+while True:
+    numbers = input().split()
+    if len(numbers) == 0:
+        continue
+    if len(numbers) == 1:
+        if numbers[0] == "/exit":
+            break
+        print(int(numbers[0]))
+    else:
+        print(int(numbers[0]) + int(numbers[1]))
+print('Bye!')
