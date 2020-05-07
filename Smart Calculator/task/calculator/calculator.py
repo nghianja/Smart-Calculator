d705573de9f9f47bd6c57cb486816f96c34ab402
James Hian Ng <james.ng@projapps.com>
2020-05-08 00:15:35 +0800

    Completed Python Stage #5

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -3,23 +3,32 @@ def is_minus(token):
     return token[0] == '-' and len(token) % 2 == 1
 
 
+def print_sum(numbers):
+    total = int(numbers[0])
+    for i in range(1, len(numbers), 2):
+        if is_minus(numbers[i]):
+            total -= int(numbers[i + 1])
+        else:
+            total += int(numbers[i + 1])
+    print(total)
+
+
 while True:
-    numbers = input().split()
-    if len(numbers) == 0:
+    line = input()
+    if len(line) == 0:
         continue
-    if len(numbers) == 1:
-        if numbers[0] == "/help":
+    if line[0] == '/':
+        if line == "/help":
             print('The program calculates the sum of numbers. It supports both unary and binary minus operators.')
-        elif numbers[0] == "/exit":
+        elif line == "/exit":
             break
         else:
-            print(int(numbers[0]))
+            print('Unknown command')
     else:
-        total = int(numbers[0])
-        for i in range(1, len(numbers), 2):
-            if is_minus(numbers[i]):
-                total -= int(numbers[i + 1])
-            else:
-                total += int(numbers[i + 1])
-        print(total)
+        try:
+            print_sum(line.split())
+        except ValueError:
+            print('Invalid expression')
+        except IndexError:
+            print('Invalid expression')
 print('Bye!')
